2b83c2d8b4950ef05a7e3eb809ca981a7068b803
Jacob Walker <jacob@tortugas-llc.com>
2013-07-27 15:01:16 -0500

    Basics Angular.js implementation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+var app = angular.module('main', []);
 
+// Routes
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {controller: 'RootCtrl', templateUrl: '/views/root.html'})
+        .when("/about", {controller: 'AboutCtrl', templateUrl: '/views/about.html'})
+        .when("/portfolio", {controller: 'PortfolioCtrl', templateUrl: '/views/portfolio.html'})
+});
