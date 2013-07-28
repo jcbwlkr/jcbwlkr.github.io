e4a99aa43b6e35ee7512d3fea1409e8a6666ec01
Jacob Walker <jacob@tortugas-llc.com>
2013-07-28 18:07:58 -0500

    Style updates and slideshow on home page.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+// Hook in angular stuff
 var app = angular.module('main', []);
 
 // Routes
 app.config(function($routeProvider) {
     $routeProvider
-        .when("/", {controller: 'RootCtrl', templateUrl: '/views/root.html'})
+        .when("/", {controller: 'HomeCtrl', templateUrl: '/views/home.html'})
         .when("/about", {controller: 'AboutCtrl', templateUrl: '/views/about.html'})
         .when("/portfolio", {controller: 'PortfolioCtrl', templateUrl: '/views/portfolio.html'})
 });
