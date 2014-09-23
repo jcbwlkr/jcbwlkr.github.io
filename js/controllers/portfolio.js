12cf79bfa28957191b458f09167561128822645f
Jacob Walker <jacob@tortugas-llc.com>
2014-09-22 22:22:55 -0500

    Hook up portfolio page

--- js/controllers/portfolio.js
+++ js/controllers/portfolio.js
@@ -1,19 +1,17 @@
 function PortfolioCtrl($scope, $routeParams, $http) {
-    $scope.projects = [
-        { name: "jacob-walker.com",          id: 1},
-        { name: "yagl-JS",                   id: 2},
-        { name: "Regular Expressions 101",   id: 3},
-        { name: "find.torrent",              id: 4},
-        { name: "devICT",                    id: 5},
-        { name: "D&D Campaign Journal",      id: 6},
-        { name: "TimeIPS",                   id: 7},
-        { name: "Natural Organic Warehouse", id: 8},
-        { name: "GeoServices API",           id: 9},
-        { name: "My Dotfiles",               id: 10},
-        { name: "Vim Config",                id: 11},
-    ];
+    $http
+        .get('/api/projects.json')
+        .success(function(data) {
+            $scope.projects = data.result;
+        });
 
-    $scope.currentProject = $http({ method: 'GET', url: '/views/projects/' + parseInt($routeParams.id) + '.html' });
+    if ($routeParams.id) {
+        $http
+            .get('/api/projects/' + $routeParams.id + '.json')
+            .success(function(data) {
+                $scope.currentProject = data.result;
+            });
+    }
 
     $scope.projectClass = function(id) {
         if (id == $routeParams.id) {
