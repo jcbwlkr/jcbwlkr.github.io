fedb15932d2adeea535f3c1dc2332f2c44f028c8
Jacob Walker <jacob@tortugas-llc.com>
2014-10-09 11:13:33 -0500

    Sort blog posts by date

--- js/controllers/blog.js
+++ js/controllers/blog.js
@@ -10,7 +10,12 @@ function BlogCtrl($scope, $routeParams, $http) {
         $http
             .get('/api/posts.json') // TODO request a smaller version of this
             .success(function(data) {
-                // TODO sort by date
+                // Sort by date with newest posts at the top
+                data.result.sort(function(a, b) {
+                    a = new Date(a.date);
+                    b = new Date(b.date);
+                    return a == b ? 0 : a > b ? -1 : 1;
+                });
                 $scope.posts = data.result;
             });
     }
