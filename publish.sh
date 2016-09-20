932a7ed332275738681b4fb7fb4b61bb73b32c6e
Jacob Walker <jacobwalker0814@gmail.com>
2016-09-19 21:37:08 -0500

    Prepare to publish to github

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!bash -ex
+
+# Clone the deploy branch
+rm -rf public
+git clone --depth=3 --branch=master git@github.com:jcbwlkr/jcbwlkr.github.io.git public
+
+# Cleanse out what's there including hidden files (but not .git). Ideally we'd
+# just use hugos flag to clean the destination dir but it would wipe out the
+# .git folder too
+find public -mindepth 1 -maxdepth 1 -not -name ".git" -exec rm -rf {} \;
+
+# Generate the updated site
+hugo -d public
+
+# Stage, commit, and push
+cd public
+git status
+git add -A
+git commit -am "Site update for $(date)"
+git push origin master
+
+cd ..
